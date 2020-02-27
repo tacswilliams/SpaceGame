1630b631c2026340d018910c79a22a0009add298
tacswilliams <55443380+tacswilliams@users.noreply.github.com>
2020-02-27 11:49:56 -0500

    Rocket and Asteroid Moves

---
+++ SpaceGame/Assets/Asteroid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public Vector3 endPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPos = new Vector3(-30, transform.position.y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+                                          endPos, 
+                                          Time.deltaTime * moveSpeed);
+    }
+}
---
+++ SpaceGame/Assets/Rocket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public Vector3[] rocketPos;
+    public int currPosIndex = 1;
+    public float t = 0;
+    public float moveSpeed = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = Time.deltaTime * moveSpeed;
+        transform.position = Vector3.Lerp(transform.position, rocketPos[currPosIndex], t);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currPosIndex = currPosIndex + 1;
+            if(currPosIndex > rocketPos.Length - 1)
+            {
+                currPosIndex = rocketPos.Length - 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currPosIndex = currPosIndex - 1;
+            if(currPosIndex < 0)
+            {
+                currPosIndex = 0;
+            }
+        }
+    }
+}
